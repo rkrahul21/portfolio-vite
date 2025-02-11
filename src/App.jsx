db9d878bacace989e700902b91ac73253fe0252d
rahul <krahul8197@gmail.com>
2025-02-11 16:35:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./componenets/Navbar";
+
+export default function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      Lorem ipsum dolor sit amet.
+
+      <Navbar />
+    </div>
+  )
+}
---
+++ src/componenets/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#1a8] text-white p-4">
+      kfegnlskbfs
+    </div>
+  )
+}
+
+export default Navbar
